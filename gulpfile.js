46f79642d6d48591dfc153f8425f51861d75e924
Anatoly <anatolijdegtyar@yandex.ru>
2021-03-08 13:44:53 +0300

    Добавляет gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const ghPages = require('gh-pages');
+const path = require('path');
+
+//gh-pages
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), './source'), cb);
+}
+
+exports.deploy = deploy;
